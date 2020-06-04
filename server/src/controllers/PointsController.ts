5ef6d648881749432460647c86dc8a3ff50caf7c
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-04 00:20:54 -0300

    Adding index points method

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,24 @@ import {Request, Response} from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+    async Index(req: Request, res: Response){
+        const { city, uf, items} = req.query;
+
+        const parsedItems = String(items)
+            .split(',')
+            .map(item => Number(item.trim()));
+
+        const points = await knex('points')
+            .join('point_items', 'points.id', '=', 'point_items.point_id')
+            .whereIn('point_items.item_id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('points.*');
+
+        return res.json(points);
+    }
+
     async ShowPoint(req: Request, res: Response) {
         const { id } = req.params;
 
@@ -57,6 +75,8 @@ class PointsController {
     
         await trx('point_items').insert(pointItems);
     
+        await trx.commit();
+
         return res.json({
             id: point_id,
             ...point,
