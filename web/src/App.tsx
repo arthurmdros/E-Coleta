b5e1467c12ddc89c1db62e6260d7cb37836deb5a
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-04 22:13:53 -0300

    Importing Home component in App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  return (  
-    <div>      
-      <Header title="E-Coleta" />        
-      <h1>Conteúdo da aplicação</h1>  
-    </div>        
+  return (         
+      <Home />
   );
 }
 
