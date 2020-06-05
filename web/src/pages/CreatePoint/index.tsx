bbc868b5d025aecd46ddcdd9eb3c969a422d1142
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-05 01:25:32 -0300

    Adding handleSubmitForm

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, Marker, TileLayer} from 'react-leaflet';
 import axios from 'axios';
@@ -24,6 +24,8 @@ interface IBGECityResponse {
 }
 
 const CreatePoint = () => {
+    const history = useHistory();
+
     const [items, setItems] = useState<Item[]>([]);
     const [ufs, setUfs] = useState<string[]>([]);
     const [cities, setCities] = useState<string[]>([]);
@@ -112,6 +114,34 @@ const CreatePoint = () => {
         }
     }
 
+    async function handleSubmit(event: FormEvent){
+        event.preventDefault();
+
+        const { name, email, whatsapp } = formData;
+        const uf = selectedUf;
+        const city = selectedCity;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems;
+        
+        const data = {
+            name,
+            email,
+            whatsapp,
+            uf,
+            city,
+            latitude,
+            longitude,
+            items
+        };
+
+        await api.post('points', data);
+
+        alert('Ponto de coleta criado');
+
+        history.push('/');
+    }
+
+
     return(
         <div id="page-create-point">
             <header>
@@ -123,7 +153,7 @@ const CreatePoint = () => {
                 </Link>
             </header>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro de <br/> ponto de coleta</h1>
 
                 <fieldset>
