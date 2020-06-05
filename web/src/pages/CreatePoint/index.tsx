50f156aaa81c8f5a1a157d8fe56b0e6563118146
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-04 22:56:11 -0300

    Adding html components and importing tags of libraries

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
-    return <h1>Hello Planet</h1>;
+    return(
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="E-Coleta"/>
+
+                <Link to='/'>
+                    <FiArrowLeft />
+                    Voltar para home
+                </Link>
+            </header>
+        </div>
+    );
 };
 
 export default CreatePoint;
