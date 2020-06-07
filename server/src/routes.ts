efc4e426ddaaf4deb9962d465a28962b231e3ae5
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-06 22:16:34 -0300

    Importing configuration of multer in routes to create collect points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,16 +1,21 @@
 import express from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
 routes.get('/points', pointsController.Index);
-routes.post('/points', pointsController.CreatePoint);
+routes.post('/points', upload.single('image') , pointsController.CreatePoint);
 routes.get('/points/:id', pointsController.ShowPoint);
 
 export default routes;
