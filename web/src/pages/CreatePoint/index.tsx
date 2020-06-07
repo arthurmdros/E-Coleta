a2ee37ed0edbfa8ffddca127b13af1ae47b0de95
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-06 23:25:36 -0300

    Sending data of type Form Data to API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -43,6 +43,7 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
@@ -124,16 +125,20 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
         
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
+        const data = new FormData();
+        
+        data.append('name', name);            
+        data.append('email', email);            
+        data.append('whatsapp', whatsapp);            
+        data.append('uf', uf);            
+        data.append('city', city);            
+        data.append('latitude', String(latitude));            
+        data.append('longitude', String(longitude));            
+        data.append('items', items.join(','));
+                    
+        if (selectedFile) {
+            data.append('image', selectedFile);
+        }
 
         await api.post('points', data);
 
@@ -157,7 +162,7 @@ const CreatePoint = () => {
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro de <br/> ponto de coleta</h1>
 
-                <Dropzone />
+                <Dropzone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>
